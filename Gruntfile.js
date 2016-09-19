0a1e37e351433207cb7364e2a8579a3284984a42
danny <dannywang@augmentum.com.cn>
2016-09-19 16:35:51 +0800

    try to understand grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -5,23 +7,27 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd H:M:ss") %> */\n'
       },
       build: {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    clean: {
+      test: ['build/**']
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('build', ['uglify']);
-  // A very basic default task.
-  // grunt.registerTask('default', 'Log some stuff.', function() {
-  //   grunt.log.write('Logging some stuff...').ok();
-  // });
+  grunt.registerTask('default', ['clean', 'uglify']);
+  // A very basic customer task.
+  grunt.registerTask('customer', 'Log some stuff.', function() {
+    grunt.log.write('Logging some stuff...').ok();
+  });
+
 };
--- src/test-grunt.js
+++ src/test-grunt.js
@@ -1,3 +1,9 @@
-(function() {
-  var username = 'danny';
-})()
+var username = 'danny';
+console.log(username.length);
+var person = {
+  name: username,
+  age: 33,
+  fn: function(n) {
+    return n*n;
+  }
+};
